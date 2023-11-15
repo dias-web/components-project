f6b6bc99dc7f56e8eaeea8563b8503a8cc2a2f71
unknown <l1verpoolg24@gmail.com>
2023-11-15 08:59:18 +0600

    refac: changed the folder structure in the project and installed all necessary dependencies

---
+++ public/index.php
@@ -0,0 +1,72 @@
+<?php
+
+use Delight\Auth\Auth;
+use League\Plates\Engine;
+use function Tamtamchik\SimpleFlash\flash;
+
+if( !session_id() ) {
+    session_start();
+}
+
+require '../vendor/autoload.php';
+use DI\ContainerBuilder;
+$containerBuilder = new ContainerBuilder;
+$containerBuilder->addDefinitions([
+    Engine::class => function() {
+        return new Engine('../app/views');
+    },
+
+    PDO::class => function() {
+        $driver = 'mysql';
+        $host = 'localhost';
+        $database_name = 'mindset';
+        $username = 'root';
+        $password = '';
+
+        return new PDO("$driver:host=$host;dbname=$database_name", $username, $password);
+
+    },
+
+    Auth::class => function($container) {
+        return new Auth($container->get('PDO'));
+    }
+]);
+$container = $containerBuilder->build();
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', ['App\controllers\UserController', 'index']);
+
+
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        echo '404';
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        echo '405';
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        $container->call($routeInfo[1], $routeInfo[2]);
+
+        break;
+}
+
+
+
